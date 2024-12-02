1aa322dbcf3c0f625034714cd8bbd297447fbc7a
rubences <rjuarcad@uax.es>
2024-12-02 19:06:26 +0100

    Segunda parte con ExecutorServiceCallable.java

--- src/ExecutorServiceCallable.java
+++ src/ExecutorServiceCallable.java
@@ -1,28 +1,38 @@
 import java.time.Duration;
 import java.time.Instant;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
-public class ExecutorServiceRunnable {
+public class ExecutorServiceCallable {
 
     private static final Instant INICIO = Instant.now();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
 
         ExecutorService executor = Executors.newSingleThreadExecutor();
 
-        Runnable tarea= ()->{
+        Callable<String> tarea= ()->{
             Log("Inicio de la tarea");
             try {
-                TimeUnit.SECONDS.sleep(1);
+                TimeUnit.SECONDS.sleep(2);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             Log("Finaliza la tarea");
+            return "Resultado de la tarea";
         };
 
-        executor.submit(tarea);
+        Future<String> future = executor.submit(tarea);
+
+        Log(future.isDone());
+        String resultado = future.get();
+        Log(future.isDone());
+
+        Log(resultado);
         executor.shutdown();
     }
 
