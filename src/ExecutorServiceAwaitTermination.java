09bfb6905933c60773e66c2be44436cdd83b4bff
rubences <rjuarcad@uax.es>
2024-12-02 19:12:21 +0100

    Tercera parte ExcecutorServiceAwaitTermination.java

--- src/ExecutorServiceAwaitTermination.java
+++ src/ExecutorServiceAwaitTermination.java
@@ -1,39 +1,33 @@
 import java.time.Duration;
 import java.time.Instant;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
-public class ExecutorServiceCallable {
+public class ExecutorServiceAwaitTermination {
 
     private static final Instant INICIO = Instant.now();
 
-    public static void main(String[] args) throws InterruptedException, ExecutionException {
+    public static void main(String[] args) throws InterruptedException {
 
         ExecutorService executor = Executors.newSingleThreadExecutor();
 
-        Callable<String> tarea= ()->{
+        Runnable tarea= ()->{
             Log("Inicio de la tarea");
             try {
-                TimeUnit.SECONDS.sleep(2);
+                TimeUnit.SECONDS.sleep(5);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             Log("Finaliza la tarea");
-            return "Resultado de la tarea";
         };
 
-        Future<String> future = executor.submit(tarea);
+        executor.submit(tarea);
 
-        Log(future.isDone());
-        String resultado = future.get();
-        Log(future.isDone());
-
-        Log(resultado);
         executor.shutdown();
+        executor.awaitTermination(2, TimeUnit.SECONDS);
+
+        Log("El hilo principal continúa...");
     }
 
     private static void Log(Object mensaje) {
